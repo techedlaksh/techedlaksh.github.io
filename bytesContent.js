f998ca11c9457fd6d93a8042c494151496ce5d68
Laksh Arora <laksharora28@gmail.com>
2025-01-06 00:20:07 +0530

    added: WIP new byte content

--- bytesContent.js
+++ bytesContent.js
@@ -1,36 +1,55 @@
-let byteText = ''
+let byteList = []
 
 function createLink(link) {
-  return link && 'bytes/' + link + '.html'
+    return link && 'bytes/' + link + '.html'
 }
 
 function addByte(title='', link='') {
-  let article = ''
-  let verticalGap = title && '<br><br>'
-  let linkUpdated = createLink(link)
-  article += `<a href=${linkUpdated}> ${title} </a>`
-  article += verticalGap
-  console.log('Article added: ', title, link)
-  return article
+    let article = ''
+    let verticalGap = title && '<br><br>'
+    let linkUpdated = createLink(link)
+    article += `<a href=${linkUpdated}> ${title} </a>`
+    article += verticalGap
+    console.log('Article added: ', title, link)
+    return article
 }
 
-// byteText += addByte(
+//byteList.push(addByte(
 //   'Byte Starter', 
 //   'byte_starter'
-// )
+//))
 
-byteText += addByte(
-  'Oh My Setup: A Config Story', 
-  'oh_my_terminal'
-)
+byteList.push(addByte(
+    'Oh My Terminal: Config files and more', 
+    'oh_my_terminal'
+))
 
-byteText += addByte()
+//byteList.push(addByte(
+//    'Vera Molnar and my time on her last project',
+//    'working_with_vera'
+//))
+//
+//byteList.push(addByte(
+//    'Coding outside the box: Working with this Artist changed my coding',
+//    'working_with_artist'
+//))
+/*
+    * Project list with Martin Grasser
+    * Squares July 22
+    * Love November 22
+    * Vera July 23
+    * Portraits December 23
+    * Bright Moments February 24
+    * Jeff Davis May 24
+    * Mirrors August 24
+*/
 
 let byteContent = `
-${byteText}
+${byteList.reverse().join('')}
 `
-let byteElement = document.getElementById('main')
-let byteInner =  document.createElement('div')
-byteInner.innerHTML = byteContent
-byteElement.appendChild(byteInner)
+
+let byteContainer = document.getElementById('main')
+let byteElement =  document.createElement('div')
+byteElement.innerHTML = byteContent
+byteContainer.appendChild(byteElement)
 
